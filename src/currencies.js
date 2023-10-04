7f8f8bffa5cf12fc7dcc0ae61efd11ff1527ec4b
Patryk Wernerowicz <patryk.wernerowicz1@gmail.com>
2023-10-04 20:22:48 +0200

    short changed

--- src/currencies.js
+++ src/currencies.js
@@ -1,6 +1,6 @@
 export const currencies = [
-    { short: "eur", name: "Euro (€)", rate: 4.63 },
-    { short: "usd", name: "Dolar amerykański ($)", rate: 4.38 },
-    { short: "gbp", name: "Funt brytyjski (£)", rate: 5.34 },
-    { short: "chf", name: "Frank szwajcarski (₣)", rate: 4.78 }
+    { short: "€", name: "Euro (€)", rate: 4.63 },
+    { short: "$", name: "Dolar amerykański ($)", rate: 4.38 },
+    { short: "£", name: "Funt brytyjski (£)", rate: 5.34 },
+    { short: "₣", name: "Frank szwajcarski (₣)", rate: 4.78 }
 ];
